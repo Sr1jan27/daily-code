8c76e79f33704676049c93875c17e0a1a757c871
srijan_singh <singhsrijan809@gmail.com>
2024-08-31 17:58:52 +0530

    substraction in any base

---
+++ 28-08-2024/SubstractionInAnyBase.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class SubstractionInAnyBase {
+    
+    
+    public static void main(String[] args) {
+        // Addition of two numbers in any base given 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the base in which it is ");
+        int b = sc.nextInt();
+        System.out.println("Enter a number in same base");
+        int x = sc.nextInt();
+        System.out.println("Enter another number to add");
+        int y = sc.nextInt();
+
+        int valueOfSubstraction = substractInAnyBase(x,y,b);
+        System.out.println("After addition answer is --> "+ valueOfSubstraction);
+        sc.close();
+    }
+
+
+    private static int substractInAnyBase(int x, int y, int b) {
+        int convertedNumberx = convertToDecimal(x, b);
+        int convertedNumbery = convertToDecimal(y, b);
+        int decimalAnswer = convertedNumberx-convertedNumbery;
+        return decimalToAnyBase(decimalAnswer, b);
+    }
+
+
+    public static int convertToDecimal(int x, int base){
+        
+        int convertedNumber = 0;
+        int a =0;
+        while(x>0){
+            int rem = x%10;
+            convertedNumber = convertedNumber +rem*(int)Math.pow(base,a);
+            a++;
+            x/=10;
+        }
+        
+        return convertedNumber;
+        
+    }
+
+    public static int decimalToAnyBase(int x, int base){
+        
+        int convertedNumber = 0;
+        int a =0;
+        while(x>0){
+            int rem = x%base;
+            convertedNumber = convertedNumber +rem*(int)Math.pow(10,a);
+            a++;
+            x/=base;
+        }
+        
+        return convertedNumber;
+        
+    }
+}
