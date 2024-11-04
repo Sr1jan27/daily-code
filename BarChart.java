8254b6bda7f2181b4b0b399f254a5aabe4fda71e
Sr1jan27 <80779925+Sr1jan27@users.noreply.github.com>
2024-11-04 23:33:18 +0530

    Update BarChart.java

--- BarChart.java
+++ BarChart.java
@@ -24,8 +24,7 @@ public class BarChart {
                 }
             }
             System.out.println();
-            
-            System.out.println();
+           
             System.out.println();
             
             System.out.println();
