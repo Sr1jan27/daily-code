42f08cb1cc19cb942fd505c819929c3a785f3b77
Srijan Singh <80779925+Sr1jan27@users.noreply.github.com>
2025-02-26 21:13:51 +0530

    Update BarChart.java

--- BarChart.java
+++ BarChart.java
@@ -8,7 +8,7 @@ public class BarChart {
 
         int[] a = new int[n];
         int max = 0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){  
             System.out.println("Enter Bar chart value");
             a[i] = sc.nextInt();
             if(max<a[i]) max = a[i];
