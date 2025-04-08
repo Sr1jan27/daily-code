b73183f93081446b5ca42f160e5a9c548b9cc0b2
Srijan Singh <80779925+Sr1jan27@users.noreply.github.com>
2025-04-08 18:28:09 +0530

    Update BarChart.java

--- BarChart.java
+++ BarChart.java
@@ -12,6 +12,7 @@ public class BarChart {
             System.out.println("Enter Bar chart value");
             
             
+            
             a[i] = sc.nextInt();
             
             if(max<a[i]) max = a[i];
