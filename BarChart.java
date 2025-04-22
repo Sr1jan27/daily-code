1cb05a377ed928a6bb3bf322ff862e0102ef07d8
Srijan Singh <80779925+Sr1jan27@users.noreply.github.com>
2025-04-22 19:36:56 +0530

    Update BarChart.java

--- BarChart.java
+++ BarChart.java
@@ -13,6 +13,8 @@ public class BarChart {
             a[i] = sc.nextInt();
             if(max<a[i]) max = a[i];
         }
+
+        
         for(int j=max;j>0;j--){
             for(int i=0;i<n;i++){
                 if(j<=a[i]){
